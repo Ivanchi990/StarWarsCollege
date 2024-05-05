9e547a89a926314bd38c03e152012447dea73715
Ivan <ivanchipablo@gmail.com>
2024-05-05 14:11:07 +0200

    Fixed

--- src/entities/Game.java
+++ src/entities/Game.java
@@ -2,9 +2,10 @@ package entities;
 
 import entities.locations.Location;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Game
+public class Game implements Serializable
 {
     private Player player1;
     private Player player2;
--- src/entities/ships/ManageShip.java
+++ src/entities/ships/ManageShip.java
@@ -1,10 +1,5 @@
 package entities.ships;
 
-import entities.Coordinates;
-import entities.crewmembers.CrewMember;
-
-import java.util.TreeMap;
-
 public interface ManageShip
 {
     boolean moveShip();
